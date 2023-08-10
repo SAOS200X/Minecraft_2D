f7640460b0a121a13e2dfc6559006b01c73f9024
SAOS <Infdresta@gmail.com>
2023-08-11 01:18:50 +0700

    add include

--- Core/pch.h
+++ Core/pch.h
@@ -7,12 +7,23 @@
 #include <map>
 #include <sstream>
 
+#include <variant>
+
 #include "SFML/Graphics.hpp"
 #include <Windows.h>
 
-#include "s_System.h"
+
+#include "stuff.h"
+
+
+
+#if 0
+#define _MY_DEBUG_ DebugHandle dd(__FUNCTION__);
+#else
+#define _MY_DEBUG_
+#endif
 
 #define logERROR(x) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12), std::cout << "ERROR" << " -> " << __FUNCTION__ << " -> " << __LINE__ << " -> " << x << "\n"
 #define logWARNING(x) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14), std::cout << "WARNING" << " -> " << __FUNCTION__ << " -> " << __LINE__ << " -> " << x << "\n"
-#define logMSG(x) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10), std::cout << "MSG" << " -> " << __FUNCTION__ << " -> " << __LINE__ << " -> " << x << "\n"
+#define logMSG(x) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15), std::cout << "MSG" << " -> " << __FUNCTION__ << " -> " << __LINE__ << " -> " << x << "\n"
 

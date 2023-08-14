20613a53095f63aea933dc3ccc029287e05da446
SAOS <Infdresta@gmail.com>
2023-08-14 23:30:44 +0700

    save file

--- Core/NewWorldState.cpp
+++ Core/NewWorldState.cpp
@@ -1,16 +1,20 @@
 #include "pch.h"
 #include "NewWorldState.h"
 #include "systemHandle.h"
+#include "SinglePlayerState.h"
 
 NewWorldState::NewWorldState()
 {
 	this->initBackground(m_path::bg_newworld);
 	initTextBoxs();
+	initButton();
 }
 
 NewWorldState::~NewWorldState()
 {
 	for (auto& i : textBoxs)
+		delete i.second;
+	for (auto& i : buttons)
 		delete i.second;
 }
 
@@ -22,12 +26,19 @@ void NewWorldState::update()
 		if (systemHandle::getUTFCode())
 			i.second->typed(systemHandle::getUTFCode());
 	}
+
+	for (auto& i : buttons)
+		i.second->update(static_cast<sf::Vector2f>(systemHandle::getMousePosWindow()),systemHandle::isButtonPressed(sf::Mouse::Left));
+
+	updateButtonActive();
 }
 
 void NewWorldState::render(sf::RenderTarget* target)
 {
 	target->draw(this->sprite);
 	for (auto& i : textBoxs)
+		i.second->render(target);
+	for (auto& i : buttons)
 		i.second->render(target);
 }
 
@@ -36,6 +47,66 @@ void NewWorldState::initTextBoxs()
 	textBoxs.insert({ "NAME", new TextBox(sf::Vector2f(400.f, 40.f), sf::Vector2f(systemHandle::getWindow()->getSize().x / 2.f, systemHandle::getWindow()->getSize().y * 2.f / 7.f),
 		sf::Color::Black, systemHandle::getFont(), 24) });
 
-	textBoxs.insert({ "SEED", new TextBox(sf::Vector2f(150.f, 40.f), sf::Vector2f(systemHandle::getWindow()->getSize().x / 2.f, systemHandle::getWindow()->getSize().y * 4.f / 7.f),
+	textBoxs.insert({ "SEED", new TextBox(sf::Vector2f(150.f, 40.f), sf::Vector2f(systemHandle::getWindow()->getSize().x / 2.f, systemHandle::getWindow()->getSize().y * 3.f / 7.f),
 		sf::Color::Black, systemHandle::getFont(), 24) });
+}
+
+void NewWorldState::initButton()
+{
+	buttons.insert({ "CREATE",new Button(systemHandle::getTexture(m_path::button_blank), sf::Vector2f(systemHandle::getWindow()->getSize().x / 2.f, systemHandle::getWindow()->getSize().y * 6.f / 7.f), 
+		systemHandle::getFont(), 24, "Create New World") });
+}
+
+void NewWorldState::updateButtonActive()
+{
+	for (auto& i : buttons)
+		if (i.second->isButtonPressed())
+		{
+			if (i.first == "CREATE")
+			{
+				createNewWorld();
+				return;
+			}
+		}
+}
+
+void NewWorldState::createNewWorld()
+{
+	if (textBoxs.at("NAME")->getString().size())
+	{
+		this->states->pop();
+		auto i = static_cast<SinglePlayerState*>(this->states->top());
+		std::stringstream filePath;
+		filePath << "resource/saves/save_" << i->saves.size() << ".save";
+
+		std::ofstream OUTFILE(m_path::save_global);
+		if (OUTFILE.is_open())
+		{
+			OUTFILE << i->saves.size() + 1 << "\n";
+
+			for (auto& j : i->saves)
+				OUTFILE << j.getSaves().name << "\n" << j.getSaves().filePath << "\n";
+			OUTFILE << textBoxs.at("NAME")->getString() << "\n" << filePath.str() << "\n";
+
+			OUTFILE.close();
+		}
+		else
+			logERROR("Couldn't save global save: " + std::string(m_path::save_global));
+
+		
+		OUTFILE.open(filePath.str());
+		if (OUTFILE.is_open())
+		{
+			OUTFILE << textBoxs.at("NAME")->getString() << "\n";
+			OUTFILE << textBoxs.at("SEED")->getString() << "\n";
+
+			OUTFILE.close();
+		}
+		else
+			logERROR("couldn't save file at: " + filePath.str());
+
+		i->loadGlobalSave(m_path::save_global);
+
+		delete this;
+	}
 }
--- Core/NewWorldState.h
+++ Core/NewWorldState.h
@@ -15,8 +15,15 @@ public:
 
 private:
     void initTextBoxs();
+    void initButton();
+
+    void updateButtonActive();
+
+    void createNewWorld();
 
 
     std::map<std::string, TextBox*> textBoxs;
+    std::map<std::string, Button*> buttons;
+
 };
 
